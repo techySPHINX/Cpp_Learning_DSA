02a445bbbfe056cc4144a5a5755cb8d29cc76dac
techySphinx <shimon123universe@gmail.com>
2024-06-20 12:15:06 +0530

    feat: rectangular and traingular

--- pattern_logics/alternate1_2.cpp
+++ pattern_logics/alternate1_2.cpp
@@ -2,27 +2,24 @@
 #include "myMath"
 
 using namespace std;
-//hollow star rectangular pattern
+
 int main()
 {
     int n, m;
     cin >> n >> m;
-
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= m; j++)
         {
-            if (i == 1 || j == 1 || i == n || j == m)
+            if ((i + j) % 2 == 0)
             {
-                cout << "*";
+                cout << "1":
             }
             else
             {
-                cout << " ";
+                cout << "2";
             }
         }
         cout << endl;
     }
-
-    return 0;
 }
---
+++ pattern_logics/numerical_triangular.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "myMath" 
+
+using namespace std;
+
+//pyramidal pattern
+
+int main(){
+    
+}
